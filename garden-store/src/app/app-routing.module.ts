3157b2a69ef20e698c0af1c80556eb4de4947c6c
Aaron <aaronwebster88@gmail.com>
2019-05-02 11:10:18 -0400

    pre-dev pull, Nav Component, routing in progress

--- garden-store/src/app/app-routing.module.ts
+++ garden-store/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
+import { AuthComponent } from './auth/auth.component';
 
 const routes: Routes = [
-  { path: "home", component: HomeComponent }
+  { path: '', component: HomeComponent },
+  // { path: 'about', component:  },
+  { path: 'admin', component: AuthComponent}
 ];
 
 @NgModule({
