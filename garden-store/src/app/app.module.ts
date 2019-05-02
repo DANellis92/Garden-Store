97c8521af5961c1c7c4f6e169367c51c647f5167
Scott <scott_hoffmann1879@outlook.com>
2019-05-02 10:52:43 -0400

    created about and products component 1st

---
+++ garden-store/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ProductsComponent } from './products/products.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    ProductsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
